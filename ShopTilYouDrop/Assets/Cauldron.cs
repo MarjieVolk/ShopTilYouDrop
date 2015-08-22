42dbeae222180c5f871b9a892cad1cae9f98d8ce
Marjie <marjie.volk@gmail.com>
2015-08-22 10:47:43 -0700

    Added Cauldron and Ingredient components (mostly empty)

---
+++ ShopTilYouDrop/Assets/Cauldron.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cauldron : MonoBehaviour {
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision2D collision) {
+        Ingredient collidingIngredient = collision.collider.gameObject.GetComponent<Ingredient>();
+        if (collidingIngredient != null) {
+
+        }
+    }
+}
