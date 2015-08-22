cd9b78ddee2d302431c5d786585a68c41ebddaec
Marjie <marjie.volk@gmail.com>
2015-08-22 11:26:48 -0700

    Cauldron deletes ingredients in it!  And new demo scene

--- ShopTilYouDrop/Assets/Cauldron.cs
+++ ShopTilYouDrop/Assets/Cauldron.cs
@@ -1,20 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Cauldron : MonoBehaviour {
 
+    private Dictionary<Ingredient, float> enterTimes;
+
+    public float consumeDelaySeconds;
+
 	void Start () {
-	
+        enterTimes = new Dictionary<Ingredient, float>();
 	}
 	
 	void Update () {
-	
+	    foreach (Ingredient ingredient in enterTimes.Keys) {
+            if (Time.time - enterTimes[ingredient] >= consumeDelaySeconds) {
+                enterTimes.Remove(ingredient);
+                consumeIngredient(ingredient);
+            }
+        }
 	}
 
-    void OnCollisionEnter(Collision2D collision) {
-        Ingredient collidingIngredient = collision.collider.gameObject.GetComponent<Ingredient>();
+    void OnTriggerEnter2D(Collider2D collision) {
+        Debug.Log("Object entered collision: " + collision.gameObject);
+        Ingredient collidingIngredient = collision.gameObject.GetComponent<Ingredient>();
         if (collidingIngredient != null) {
-
+            enterTimes.Add(collidingIngredient, Time.time);
         }
+    }
+
+    void OnTriggerExit2D(Collider2D collision) {
+        Debug.Log("Object exited collision: " + collision.gameObject);
+        Ingredient collidingIngredient = collision.gameObject.GetComponent<Ingredient>();
+        if (collidingIngredient != null) {
+            enterTimes.Remove(collidingIngredient);
+        }
+    }
+
+    private void consumeIngredient(Ingredient ingredient) {
+        //TODO: add ingredient to potion
+        Destroy(ingredient.gameObject);
     }
 }
