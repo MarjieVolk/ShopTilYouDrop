32c2f3ef5348b940768b3f887d3a74fa6f4f1729
Marjie <marjie.volk@gmail.com>
2015-08-23 12:17:36 -0700

    Cleaning up the pause menu, since it was getting big.  Also redid potion panel sizing so that it's closer to reasonable, and added a remove button!

---
+++ ShopTilYouDrop/Assets/PauseMenu/PauseMenuController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class PauseMenuController : MonoBehaviour {
+
+    private PotionMenuController potionMenu;
+    private GameObject pauseMenu;
+
+	// Use this for initialization
+    void Start() {
+        potionMenu = GameObject.FindObjectOfType<PotionMenuController>();
+        pauseMenu = transform.Find("Pause Menu").gameObject;
+        pauseMenu.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.Escape)) {
+            Time.timeScale = 0;
+            pauseMenu.SetActive(true);
+            potionMenu.showPotionsTab();
+        }
+	}
+
+    public void onClickResume() {
+        Time.timeScale = 1;
+        pauseMenu.SetActive(false);
+    }
+
+    private void showIngredientsTab() {
+        
+    }
+}
