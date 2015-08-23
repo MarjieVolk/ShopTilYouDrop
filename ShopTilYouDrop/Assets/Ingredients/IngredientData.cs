0745b95346915cdbbdbb1fc664a3403e8e772cac
Marjie <marjie.volk@gmail.com>
2015-08-23 16:36:25 -0700

    Item menu works too now!!!!!!

--- ShopTilYouDrop/Assets/Ingredients/IngredientData.cs
+++ ShopTilYouDrop/Assets/Ingredients/IngredientData.cs
@@ -8,12 +8,77 @@ class IngredientData {
     public readonly Aspects.Primary primary;
     public readonly Aspects.Secondary secondary;
 
-    public Aspects.Primary primaryPlayerGuess = Aspects.Primary.UNKNOWN;
-    public Aspects.Secondary secondaryPlayerGuess = Aspects.Secondary.UNKNOWN;
+    private Dictionary<Aspects.Primary, GuessState> primaryGuesses;
+    private Dictionary<Aspects.Secondary, GuessState> secondaryGuesses;
 
     public IngredientData(IngredientType type, Aspects.Primary primary, Aspects.Secondary secondary) {
         this.type = type;
         this.primary = primary;
         this.secondary = secondary;
+
+        primaryGuesses = new Dictionary<Aspects.Primary, GuessState>();
+        secondaryGuesses = new Dictionary<Aspects.Secondary, GuessState>();
+    }
+
+    public GuessState getGuessState(Aspects.Primary aspect) {
+        if (primaryGuesses.ContainsKey(aspect)) {
+            return primaryGuesses[aspect];
+        } else {
+            return GuessState.UNKNOWN;
+        }
+    }
+
+    public GuessState getGuessState(Aspects.Secondary aspect) {
+        if (secondaryGuesses.ContainsKey(aspect)) {
+            return secondaryGuesses[aspect];
+        } else {
+            return GuessState.UNKNOWN;
+        }
+    }
+
+    public void setGuessState(Aspects.Primary aspect, GuessState state) {
+        primaryGuesses[aspect] = state;
+    }
+
+    public void setGuessState(Aspects.Secondary aspect, GuessState state) {
+        secondaryGuesses[aspect] = state;
+    }
+
+    public Aspects.Primary getPrimaryGuess() {
+        Aspects.Primary guess = Aspects.Primary.UNKNOWN;
+        foreach (Aspects.Primary aspect in primaryGuesses.Keys) {
+            if (primaryGuesses[aspect] == GuessState.HAS) {
+                if (guess == Aspects.Primary.UNKNOWN) {
+                    guess = aspect;
+                } else {
+                    // Multiple aspects guessed; invalid
+                    return Aspects.Primary.UNKNOWN;
+                }
+            }
+        }
+
+        return guess;
+    }
+
+    public Aspects.Secondary getSecondaryGuess() {
+        Aspects.Secondary guess = Aspects.Secondary.UNKNOWN;
+        foreach (Aspects.Secondary aspect in secondaryGuesses.Keys) {
+            if (secondaryGuesses[aspect] == GuessState.HAS) {
+                if (guess == Aspects.Secondary.UNKNOWN) {
+                    guess = aspect;
+                } else {
+                    // Multiple aspects guessed; invalid
+                    return Aspects.Secondary.UNKNOWN;
+                }
+            }
+        }
+
+        return guess;
+    }
+
+    public enum GuessState {
+        UNKNOWN,
+        HAS,
+        NOT_HAS
     }
 }
--- ShopTilYouDrop/Assets/PauseMenu/IngredientMenuController.cs
+++ ShopTilYouDrop/Assets/PauseMenu/IngredientMenuController.cs
@@ -1,17 +1,101 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
+using UnityEngine.UI;
 
 public class IngredientMenuController : MonoBehaviour {
 
     public GameObject lineItem;
 
-	// Use this for initialization
 	void Start () {
-	
+        foreach (Transform child in gameObject.transform) {
+            Destroy(child.gameObject);
+        }
+
+        foreach (IngredientType type in Enum.GetValues(typeof(IngredientType))) {
+            initIngredientLineItem(type);
+        }
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    private void initIngredientLineItem(IngredientType type) {
+        GameObject display = Instantiate(lineItem);
+        display.transform.parent = transform;
+
+        setSprite(display, "Ingredient", IngredientRenderer.instance().getSprite(type));
+        
+        initAspect(type, display, "Dairy", Aspects.Primary.DAIRY);
+        initAspect(type, display, "Veggie", Aspects.Primary.PLANT);
+        initAspect(type, display, "Meat", Aspects.Primary.MEAT);
+        initAspect(type, display, "Grain", Aspects.Primary.GRAIN);
+        
+        initAspect(type, display, "Fire", Aspects.Secondary.FIRE);
+        initAspect(type, display, "Water", Aspects.Secondary.WATER);
+        initAspect(type, display, "Void", Aspects.Secondary.VOID);
+        initAspect(type, display, "Slime", Aspects.Secondary.SLIME);
+        initAspect(type, display, "Decay", Aspects.Secondary.DECAY);
+        initAspect(type, display, "Beast", Aspects.Secondary.BLOOD);
+        initAspect(type, display, "Seductive", Aspects.Secondary.ANGELIC);
+    }
+
+    private void initAspect(IngredientType ingredient, GameObject lineItemInstance, String childName, Aspects.Primary aspect) {
+        GameObject iconButton = lineItemInstance.transform.Find(childName).gameObject;
+        iconButton.GetComponent<Image>().sprite = Aspects.instance().getGreyedSprite(aspect);
+
+        iconButton.GetComponent<Button>().onClick.AddListener(() => {
+            IngredientData data = Ingredients.instance().getIngredient(ingredient);
+            IngredientData.GuessState guess = data.getGuessState(aspect);
+
+            IngredientData.GuessState next = IngredientData.GuessState.UNKNOWN;
+            Image icon = iconButton.GetComponent<Image>();
+            switch (guess) {
+                case IngredientData.GuessState.UNKNOWN:
+                    next = IngredientData.GuessState.HAS;
+                    icon.sprite = Aspects.instance().getNormalSprite(aspect);
+                    break;
+                case IngredientData.GuessState.HAS:
+                    next = IngredientData.GuessState.NOT_HAS;
+                    icon.sprite = Aspects.instance().getDisabledSprite(aspect);
+                    break;
+                case IngredientData.GuessState.NOT_HAS:
+                    next = IngredientData.GuessState.UNKNOWN;
+                    icon.sprite = Aspects.instance().getGreyedSprite(aspect);
+                    break;
+            }
+
+            data.setGuessState(aspect, next);
+        });
+    }
+
+    private void initAspect(IngredientType ingredient, GameObject lineItemInstance, String childName, Aspects.Secondary aspect) {
+        GameObject iconButton = lineItemInstance.transform.Find(childName).gameObject;
+        iconButton.GetComponent<Image>().sprite = Aspects.instance().getGreyedSprite(aspect);
+
+        iconButton.GetComponent<Button>().onClick.AddListener(() => {
+            IngredientData data = Ingredients.instance().getIngredient(ingredient);
+            IngredientData.GuessState guess = data.getGuessState(aspect);
+
+            IngredientData.GuessState next = IngredientData.GuessState.UNKNOWN;
+            Image icon = iconButton.GetComponent<Image>();
+            switch (guess) {
+                case IngredientData.GuessState.UNKNOWN:
+                    next = IngredientData.GuessState.HAS;
+                    icon.sprite = Aspects.instance().getNormalSprite(aspect);
+                    break;
+                case IngredientData.GuessState.HAS:
+                    next = IngredientData.GuessState.NOT_HAS;
+                    icon.sprite = Aspects.instance().getDisabledSprite(aspect);
+                    break;
+                case IngredientData.GuessState.NOT_HAS:
+                    next = IngredientData.GuessState.UNKNOWN;
+                    icon.sprite = Aspects.instance().getGreyedSprite(aspect);
+                    break;
+            }
+
+            data.setGuessState(aspect, next);
+        });
+    }
+
+    private void setSprite(GameObject lineItemInstance, String childName, Sprite sprite) {
+        lineItemInstance.transform.Find(childName).GetComponent<Image>().sprite = sprite;
+    }
 }
