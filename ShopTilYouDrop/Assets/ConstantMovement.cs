4020ee79cf4b5393326ffafb5e4c21040c76bbf2
knexer <knexer@gmail.com>
2015-08-22 19:36:38 -0700

    Impl infinite shelf-stocking 1.0
    Working in the Taylor/InfiniteRunning scene

---
+++ ShopTilYouDrop/Assets/ConstantMovement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConstantMovement : MonoBehaviour {
+    public Vector2 Step;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.position += (Vector3)Step;
+	}
+}
