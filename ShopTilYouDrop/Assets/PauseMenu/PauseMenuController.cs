0d2b9bd48146f12fdb566c591eb48a8542f9d6f9
Marjie <marjie.volk@gmail.com>
2015-08-23 16:39:22 -0700

    Space now pauses as well

--- ShopTilYouDrop/Assets/PauseMenu/PauseMenuController.cs
+++ ShopTilYouDrop/Assets/PauseMenu/PauseMenuController.cs
@@ -16,7 +16,7 @@ public class PauseMenuController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	    if (Input.GetKeyDown(KeyCode.Escape)) {
+	    if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Space)) {
             Time.timeScale = 0;
             pauseMenu.SetActive(true);
             potionMenu.showPotionsTab();
