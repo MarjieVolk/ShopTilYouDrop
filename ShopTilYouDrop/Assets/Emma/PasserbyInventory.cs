ef1cd795a7dcdac7935d62f39f83ab941bcd6911
Emma Breen <emmabreen@Emmas-MacBook-Pro.local>
2015-08-23 22:32:16 -0700

    Adding logic for passerby to throw items at you

---
+++ ShopTilYouDrop/Assets/Emma/PasserbyInventory.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class PasserbyInventory : MonoBehaviour {
+	
+	// Manages access to the upcoming shelf space
+	// TODO There's a final backup generator for when everyone refuses to place ingredients so the shelves aren't empty
+
+	public Vector3 SpawnPosition;
+	public Vector2 Speed;
+	public float Gravity;
+	public LayerMask SpawnedObjectsLayer;
+	public GameObject IngredientUniversalPrefab;
+	
+	// The available strategies for ingredient placement
+	private IList<IIngredientGenerator> generators;
+	
+	public PasserbyInventory()
+	{
+		generators = new List<IIngredientGenerator>();
+	}
+	
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		Shuffle(generators);
+		foreach (IIngredientGenerator generator in generators)
+		{
+			IngredientType? type = generator.TryPlaceIngredient(null, 0);
+			if (type != null)
+			{
+				GameObject instantiated = Instantiate(IngredientUniversalPrefab);
+				instantiated.GetComponent<Ingredient>().type = (IngredientType)type;
+				instantiated.GetComponent<Ingredient>().InitializeGameObject();
+
+				instantiated.transform.position = SpawnPosition;
+				instantiated.GetComponent<Rigidbody2D>().isKinematic = true;
+				instantiated.AddComponent<ThrowingMovement>().Step = Speed;
+				instantiated.GetComponent<ThrowingMovement>().Gravity = Gravity;
+				instantiated.layer = SpawnedObjectsLayer.getFirstSet();
+			}
+		}
+	}
+	
+	public void registerGenerator(IIngredientGenerator generator)
+	{
+		generators.Add(generator);
+	}
+	
+	private static System.Random rng = new System.Random();
+	
+	// Fisher-Yates
+	public static void Shuffle<T>(IList<T> list)
+	{
+		int i = 0;
+		while (i < list.Count)
+		{
+			int target = rng.Next(i, list.Count);
+			T temp = list[i];
+			list[i] = list[target];
+			list[target] = temp;
+			i++;
+		}
+	}
+}
---
+++ ShopTilYouDrop/Assets/Emma/ThrowingMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThrowingMovement : MonoBehaviour {
+	public Vector2 Step;
+	public float Gravity;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.position += (Vector3)Step * Time.deltaTime;
+		Step.y -= Gravity;
+	}
+}
