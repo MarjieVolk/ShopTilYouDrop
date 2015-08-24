482d765993343ba07aeb5c2f26fee345a72c9326
knexer <knexer@gmail.com>
2015-08-24 00:18:40 -0700

    Impl music!  With fading.  TODO pausing it.
    See the InfiniteRunning scene for setup.

---
+++ ShopTilYouDrop/Assets/AudioSwitcher.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class AudioSwitcher : MonoBehaviour {
+
+    public AudioMixerSnapshot levelMusic;
+    public AudioMixerSnapshot pauseMusic;
+    public float TransitionSeconds;
+    public float TimeWarpingFactor;
+
+    private AudioMixerSnapshot current;
+
+	// Use this for initialization
+	void Start () {
+        current = levelMusic;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.timeScale <= TimeWarpingFactor)
+        {
+            if (current == levelMusic)
+            {
+                Time.timeScale = TimeWarpingFactor;
+                current = pauseMusic;
+                current.TransitionTo(TransitionSeconds * TimeWarpingFactor);
+                StartCoroutine(RepauseLater(TransitionSeconds * TimeWarpingFactor));
+            }
+        }
+        else
+        {
+            if (current == pauseMusic)
+            {
+                current = levelMusic;
+                current.TransitionTo(TransitionSeconds);
+            }
+        }
+	}
+
+    IEnumerator RepauseLater(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        if (Time.timeScale <= TimeWarpingFactor) Time.timeScale = 0;
+    }
+}
