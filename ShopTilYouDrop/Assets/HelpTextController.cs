c5b00d47f19f88d0864674b04ea9c24a3014b616
Marjie <marjie.volk@gmail.com>
2015-08-23 22:49:50 -0700

    Added some help text!

---
+++ ShopTilYouDrop/Assets/HelpTextController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HelpTextController : MonoBehaviour {
+
+    private GameObject tutorialPanel;
+    private Text title, body;
+
+	void Start () {
+        tutorialPanel = transform.Find("Help Text").gameObject;
+        tutorialPanel.SetActive(false);
+        title = tutorialPanel.transform.Find("Title").GetComponent<Text>();
+        body = tutorialPanel.transform.Find("Body").GetComponent<Text>();
+
+        tutorialPanel.transform.Find("Button").GetComponent<Button>().onClick.AddListener(() => {
+            tutorialPanel.SetActive(false);
+        });
+	}
+
+    public void showText(string title, string body) {
+        this.title.text = title;
+        this.body.text = body;
+        tutorialPanel.SetActive(true);
+    }
+}
